ccc557660b741be2442ebfc0631799a02b5c0462
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-12-21 13:03:28 -0300

    Update LivroSemCores

--- AvaliacaoTresPatternsAjustada/src/Model/LivroSemCores.java
+++ AvaliacaoTresPatternsAjustada/src/Model/LivroSemCores.java
@@ -2,5 +2,14 @@ package Model;
 
 public class LivroSemCores implements LivroBridgeIF  {
 
-	
+	@Override
+	public double getPercentLivro() {		
+		return 1;
+	}
+
+	@Override
+	public String getFormatacao() {		
+		return "Sem Cores";
+	}
+
 }
