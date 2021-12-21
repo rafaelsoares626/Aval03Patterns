9098ea3a13f770a9b0c4484c07fb8d792d81d7b4
Veronica Lemos <veulemos@gmail.com>
2021-12-21 11:33:35 -0300

    Update LivroDigital

--- AvaliacaoTresPatternsAjustada/src/Model/LivroDigital.java
+++ AvaliacaoTresPatternsAjustada/src/Model/LivroDigital.java
@@ -4,6 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LivroDigital extends Livro {
+	private Map<String, LivroBridgeIF> formatos = new HashMap<String, LivroBridgeIF>();
 	
+	public LivroDigital(String nome, String codigo, double preco, String isbn, LivroBridgeIF formato) {
+		super(nome, codigo, preco, isbn);		
+		this.addFormato(formato);	
+	}
 
+	public double getPercentLivro(){
+		return 0.85;
+	}	
+	
+	public void addFormato(LivroBridgeIF formato) {				
+		this.formatos.put(formato.getFormatacao(), formato);
+		this.preco = this.preco * formato.getPercentLivro() * this.getPercentLivro();		
+	}
+	
+	public String getFormato() {
+		String format = "";
+		for (String key : formatos.keySet()) {
+		    format = format + key;
+		}		
+		return "Livro Digital " + format;
+	}
+	
 }
